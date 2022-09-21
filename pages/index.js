b8e1bbe8691bed598a5a592e078568c25d214eb1
dimdatadevops <114044136+dimdatadevops@users.noreply.github.com>
2022-09-21 10:52:12 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -46,36 +46,15 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
       <Head>
         <title>{config.settings.title}</title>
         <link rel="stylesheet" href="./style.css" />
-        <script>
-          {`
-          function setTheme(theme) {
-            document.documentElement.classList.remove("dark", "light")
-            document.documentElement.classList.add(theme)
-            localStorage.theme = theme
-          }
-          (() => {
-            const query = window.matchMedia("(prefers-color-scheme: dark)")
-            query.addListener(() => {
-              setTheme(query.matches ? "dark" : "light")
-            })
-            if (["dark", "light"].includes(localStorage.theme)) {
-              setTheme(localStorage.theme)
-            } else {
-              setTheme(query.matches ? "dark" : "light")
-            }
-          })()
-          `}
-        </script>
       </Head>
       <div className="container mx-auto px-4">
         <div className="flex flex-row justify-between items-center p-4">
           <div className="flex flex-row items-center">
             <img className="h-8 w-auto" src={config.settings.logo} />
-            <h1 className="ml-4 text-3xl">{config.settings.title}</h1>
+            <h1 className="ml-4 text-3xl">Status</h1>
           </div>
           <div className="flex flex-row items-center">
-            {typeof window !== 'undefined' && <ThemeSwitcher />}
-            <MonitorFilter active={slash} callback={filterByTerm} />
+            
           </div>
         </div>
         <MonitorStatusHeader kvMonitorsLastUpdate={kvMonitorsLastUpdate} />
@@ -90,23 +69,12 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
         })}
         <div className="flex flex-row justify-between mt-4 text-sm">
           <div>
-            Powered by{' '}
-            <a href="https://workers.cloudflare.com/" target="_blank">
-              Cloudflare Workers{' '}
+            <a href="https://www.dimdata.com/">
+              Dimdata.com{' '}
             </a>
-            &{' '}
-            <a href="https://flareact.com/" target="_blank">
-              Flareact{' '}
-            </a>
+            
           </div>
-          <div>
-            <a
-              href="https://github.com/eidam/cf-workers-status-page"
-              target="_blank"
-            >
-              Get Your Status Page
-            </a>
-          </div>
+          
         </div>
       </div>
     </div>
